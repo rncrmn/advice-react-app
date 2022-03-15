261deace68934a8f6f2a27f0868b49db8f835bb8
leadgeex <aaronleadgeex@gmail.com>
2022-03-15 23:18:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const App = () => {
+
+    const API_URL = 'https://api.adviceslip.com/advice'
+
+    const [advice, setAdvice] = useState('')
+
+    const fetchAdvice = async () => {
+        const response = await fetch(API_URL)
+        const data = await response.json()
+
+        const { advice } = data.slip
+
+        setAdvice(advice)
+    }
+
+    useEffect(() => {
+        fetchAdvice()
+    } )
+
+    return(
+        <div className="app">
+            <div className='app-container'>
+                <h1 className='advice-content'>{advice}</h1>
+                <button className='advice-btn' onClick={ fetchAdvice }>Give me advice!</button>
+                <p className='advice-note'><strong>Note:</strong> Advice is cached for 2 seconds. Any repeat-request within 2 seconds will return the same piece of advice.</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
